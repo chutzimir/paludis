39be16b96189c70f4dd703e919eb17fa6b13933c
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-03-08 22:53:45 +0000

    Too fast

--- 0.26.0_alpha12/doc/htmltidy.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "${1/.html}" == "${1}" ]] ; then
-    echo "Not .html, no checking performed"
-    exit 0
-fi
-
-tidy -utf8 -q < "${1}" 1>/dev/null
-
--- 0.26.0_alpha12/paludis/stripper_TEST_binary.cc
+++
@@ -1,9 +0,0 @@
-/* vim: set sw=4 sts=4 et foldmethod=syntax : */
-
-#include <iostream>
-
-int main(int, char *[])
-{
-    std::cout << "What's this obsession with monkeys anyway?" << std::endl;
-}
-
