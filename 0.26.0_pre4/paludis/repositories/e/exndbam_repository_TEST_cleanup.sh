1f03944c2a2df325a17088203f48ebb8702263be
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2008-04-22 15:34:05 +0000

    dleverton fails it :(

--- 0.26.0_pre4/paludis/repositories/e/exndbam_repository_TEST_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vim: set ft=sh sw=4 sts=4 et :
-
-if [ -d exndbam_repository_TEST_dir ] ; then
-    rm -fr exndbam_repository_TEST_dir
-else
-    true
-fi
-
--- 0.26.0_pre4/paludis/repositories/e/fetch_visitor_TEST_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vim: set ft=sh sw=4 sts=4 et :
-
-if [ -d fetch_visitor_TEST_dir ] ; then
-    rm -fr fetch_visitor_TEST_dir
-else
-    true
-fi
-
