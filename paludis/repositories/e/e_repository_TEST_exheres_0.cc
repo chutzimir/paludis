e8f31428ff523cd180cc2ff48a5e36fc573ebca9
David Leverton <levertond@googlemail.com>
2013-08-25 18:22:49 +0100

    Make keepdir an external command
    PMS says it should be, and for consistency with the other commands it
    was decided to keep the spec as it is and change the implementations.
    
    Also remove the non-standard non-working attempt at supporting -r/-R.
    
    Fixes: Gentoo#482128

--- paludis/repositories/e/e_repository_TEST_exheres_0.cc
+++ paludis/repositories/e/e_repository_TEST_exheres_0.cc
@@ -303,7 +303,15 @@ TEST(ERepository, InstallExheres0)
 
     {
         const std::shared_ptr<const PackageID> id(*env[selection::RequireExactlyOne(generator::Matches(
-                        PackageDepSpec(parse_user_package_dep_spec("cat/keepdir-fail",
+                        PackageDepSpec(parse_user_package_dep_spec("=cat/keepdir-fail-1",
+                                &env, { })), make_null_shared_ptr(), { }))]->last());
+        ASSERT_TRUE(bool(id));
+        EXPECT_THROW(id->perform_action(action), ActionFailedError);
+    }
+
+    {
+        const std::shared_ptr<const PackageID> id(*env[selection::RequireExactlyOne(generator::Matches(
+                        PackageDepSpec(parse_user_package_dep_spec("=cat/keepdir-fail-2",
                                 &env, { })), make_null_shared_ptr(), { }))]->last());
         ASSERT_TRUE(bool(id));
         EXPECT_THROW(id->perform_action(action), ActionFailedError);
